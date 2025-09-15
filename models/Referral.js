a99172c0164d781ac133566f79b2c3992a182a5f
rollingstones-prog <rollingstonesltd72@gmail.com>
2025-09-15 17:01:18 +0500

    Update Referral.js

--- models/Referral.js
+++ models/Referral.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+import { Schema, model } from 'mongoose';
 
-const ReferralSchema = new mongoose.Schema({
-  referrer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  referred: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+const PayoutSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  points: { type: Number, required: true },
+  amount: { type: Number, default: 100 },
+  status: { type: String, enum: ['pending','paid'], default: 'pending' }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Referral', ReferralSchema);
+export default model('Payout', PayoutSchema);
